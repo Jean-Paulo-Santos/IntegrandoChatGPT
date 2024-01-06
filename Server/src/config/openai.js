dff1eca3c905cb6f5ef41827e8fe127115520977
Jean Paulo dos Santos <jeanpauloeng@gmail.com>
2024-01-06 14:44:41 -0300

    IntegrandoChatGPT

---
+++ Server/src/config/openai.js
@@ -0,0 +1,24 @@
+import OpenAI from "openai";
+
+module.exports = class openai {
+    static configuration() {
+        const openai = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+        return new OpenAIApi(configuration)
+    }
+    static textCompletion({ prompt }) {
+        return {
+            model: "davinci-002",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3500,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        }
+    }
+}
+
+
+
